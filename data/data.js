c80cf9430f57c3485dd384a130ad45d801665fe7
Sneha0420-svg <snehajerish@gmail.com>
2026-07-15 11:00:25 +0530

    update changes

---
+++ data/data.js
@@ -0,0 +1,141 @@
+[
+  {
+    "name": "Arun Lalchand Todarwal",
+    "DIN": "20916",
+    "gender": "Male",
+    "dob": "",
+    "phone": "",
+    "email": "",
+    "address": "",
+    "nationality": "Indian",
+    "currentDesignation": "Director",
+    "qualification": "Fellow Chartered Accountant (FCA), Institute of Chartered Accountants of India (ICAI)",
+    "companyDetails": {
+      "companyName": "Welspun Transformation Services Limited",
+      "description": "Anuh Pharma Ltd., a Bulk Drugs Manufacturing Company is part of the INR 8.5 bn SK Group of Companies, which employs 2000+ people across businesses such as Manufacturing of API's, Pharma Formulations, Distribution and Logistics. Established in 1960, Anuh Pharma Ltd. today is one of the largest manufacturers of Macrolides and Anti-TB products in India, besides being a major player in Anti-Bacterials, Anti-Malarial, and Corticosteroids.",
+      "industry": "Pharmaceuticals",
+      "location": "",
+      "cin": "U74999GJ2022PLC135356",
+      "registerNo": "11586"
+    },
+    "careerHistory": [
+      {
+        "company": "Welspun Living Limited (formerly Welspun India Ltd.)",
+        "role": "Nominee Director",
+        "joiningDate": "09-Aug-2007",
+        "resigningDate": "22-Aug-2012",
+        "status": "Resigned"
+      },
+      {
+        "company": "Anuh Pharma Limited",
+        "role": "Director",
+        "joiningDate": "16-Sep-2009",
+        "resigningDate": "",
+        "status": "Active"
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "6a4496a10d3690111910a02f"
+    },
+    "name": "Bipin Nemchand Shah",
+    "DIN": "00083244",
+    "gender": "Male",
+    "dob": "",
+    "phone": "",
+    "email": "",
+    "address": "",
+    "nationality": "Indian",
+    "currentDesignation": "Director",
+    "qualification": "Chemical Engineer",
+    "companyDetails": {
+      "companyName": "Anuh Pharma Limited",
+      "description": "Anuh Pharma Ltd., a Bulk Drugs Manufacturing Company, is part of the INR 8.5 billion SK Group of Companies...",
+      "industry": "Pharmaceuticals",
+      "location": "Mumbai, Maharashtra, India",
+      "cin": "L24230MH1960PLC011586",
+      "registerNo": "011586"
+    },
+    "careerHistory": [
+      {
+        "company": "Anuh Pharma Limited",
+        "role": "Managing Director",
+        "joiningDate": "01-Apr-2009",
+        "resigningDate": "07-Jan-2021",
+        "status": "Resigned"
+      },
+      {
+        "company": "Anuh Pharma Limited",
+        "role": "Director",
+        "joiningDate": "08-Jan-2021",
+        "resigningDate": "",
+        "status": "Active"
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "6a449b350d3690111910a030"
+    },
+    "name": "Bharat Nemchand Shah",
+    "DIN": "00083354",
+    "gender": "Male",
+    "dob": "",
+    "phone": "",
+    "email": "",
+    "address": "",
+    "nationality": "Indian",
+    "currentDesignation": "Non-Executive Non-Independent Director",
+    "qualification": "Commerce Graduate",
+    "companyDetails": {
+      "companyName": "Anuh Pharma Limited",
+      "description": "Anuh Pharma Ltd., a Bulk Drugs Manufacturing Company...",
+      "industry": "Pharmaceuticals",
+      "location": "Mumbai, Maharashtra, India",
+      "cin": "L24230MH1960PLC011586",
+      "registerNo": "011586"
+    },
+    "careerHistory": [
+      {
+        "company": "Anuh Pharma Limited",
+        "role": "Director",
+        "joiningDate": "29-Oct-1980",
+        "resigningDate": "",
+        "status": "Active"
+      }
+    ]
+  },
+  {
+    "_id": {
+      "$oid": "6a44a6600d3690111910a032"
+    },
+    "name": "Ritesh Bipin Shah",
+    "DIN": "02496729",
+    "gender": "Male",
+    "dob": "",
+    "phone": "",
+    "email": "",
+    "address": "",
+    "nationality": "Indian",
+    "currentDesignation": "Managing Director",
+    "qualification": "B.Sc. (Chemistry), MBA (Oklahoma City University, USA)",
+    "companyDetails": {
+      "companyName": "Anuh Pharma Limited",
+      "description": "Anuh Pharma Ltd., a Bulk Drugs Manufacturing Company...",
+      "industry": "Pharmaceuticals",
+      "location": "Mumbai, Maharashtra, India",
+      "cin": "L24230MH1960PLC011586",
+      "registerNo": "011586"
+    },
+    "careerHistory": [
+      {
+        "company": "Anuh Pharma Limited",
+        "role": "Managing Director",
+        "joiningDate": "08-Jan-2021",
+        "resigningDate": "",
+        "status": "Active"
+      }
+    ]
+  }
+]
